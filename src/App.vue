b9ac2c9e85d8d28704836479fa89961b1455dab3
Eugene Druzhniaev <druzhniaev2@gmail.com>
2022-02-11 21:58:03 +0600

    add filter

--- src/App.vue
+++ src/App.vue
@@ -93,12 +93,15 @@
         <div>
           <button
             class="my-4 mx-2 inline-flex items-center py-2 px-4 border border-transparent shadow-sm text-sm leading-4 font-medium rounded-full text-white bg-gray-600 hover:bg-gray-700 transition-colors duration-300 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-500"
+            v-if="page > 1"
+            @click="page = page - 1"
           >
             Назад
           </button>
           <button
             class="my-4 mx-2 inline-flex items-center py-2 px-4 border border-transparent shadow-sm text-sm leading-4 font-medium rounded-full text-white bg-gray-600 hover:bg-gray-700 transition-colors duration-300 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-500"
-            disabled
+            @click="page += page"
+            v-if="hasNextPage"
           >
             Вперед
           </button>
@@ -107,7 +110,7 @@
         <hr class="w-full border-t border-gray-600 my-4" />
         <dl class="mt-5 grid grid-cols-1 gap-5 sm:grid-cols-3">
           <div
-            v-for="t in filteredList()"
+            v-for="t in filteredTickers()"
             :key="t.name"
             @click="select(t)"
             :class="{
@@ -203,6 +206,7 @@ export default {
       graph: [],
       page: 1,
       filter: "",
+      hasNextPage: true,
     };
   },
 
@@ -218,8 +222,14 @@ export default {
   },
 
   methods: {
-    filteredList() {
-      return this.tickers.filter((ticker) => ticker.name.includes(this.filter));
+    filteredTickers() {
+      const start = (this.page - 1) * 6;
+      const end = this.page * 6;
+      const filteredTickers = this.tickers.filter((ticker) =>
+        ticker.name.includes(this.filter)
+      );
+      this.hasNextPage = filteredTickers.length > end;
+      return filteredTickers.slice(start, end);
     },
 
     subscribeToUpdates(ticker) {
@@ -246,6 +256,7 @@ export default {
       localStorage.setItem("crypto-list", JSON.stringify(this.tickers));
 
       this.ticker = "";
+      this.filter = "";
     },
     handleDelete(tickerToRemove) {
       this.tickers = this.tickers.filter((t) => t !== tickerToRemove);
@@ -262,6 +273,11 @@ export default {
     select(ticker) {
       this.sel = ticker;
       this.graph = [];
+    },
+  },
+  watch: {
+    filter() {
+      this.page = 1;
     },
   },
 };
